b510d99eba097bb0490dcb63040c8cfdd053aa50
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-04-05 16:27:16 -0400

    color code activity items

--- components/activity/activity-card.tsx
+++ components/activity/activity-card.tsx
@@ -3,35 +3,47 @@
 import { GetActivityItem } from '@/types';
 import { format } from 'date-fns';
 import { Badge } from '../ui/badge';
+import { Info } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
 const formatItemType = (type: string) => {
 	let lbl = '';
+	let css = '';
 
 	switch (type) {
 		case 'logFood':
 			lbl = 'Log Update';
+			css = 'text-amber-300';
 			break;
 		case 'noteData':
 			lbl = 'Notes';
+			css = 'text-slate-600';
 			break;
 		case 'foodSearch':
 			lbl = 'Search';
+			css = 'text-amber-600';
 			break;
 		case 'groceryData':
 			lbl = 'Groceries';
+			css = 'text-green-600';
 			break;
 	}
 
-	return lbl;
+	return { label: lbl, css };
 };
 
 export default function ActivityCard({ item }: { item: GetActivityItem }) {
 	return (
 		<div className='rounded-md border-2 p-2 text-xs'>
 			<div className='flex flex-row items-center justify-between pb-2'>
-				<div className='capitalize'>
-					<span className='text-foreground font-semibold'>
-						{formatItemType(item.type)}
+				<div className='capitalize flex flex-row items-center gap-1'>
+					<Info className={cn('w-4 h-4', formatItemType(item.type).css)} />
+					<span
+						className={cn(
+							'text-foreground font-semibold',
+							formatItemType(item.type).css
+						)}>
+						{formatItemType(item.type).label}
 					</span>
 				</div>
 				<Badge
