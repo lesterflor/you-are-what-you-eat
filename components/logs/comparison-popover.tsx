a5929e7eeef44798844708e5cda3fb69063b661b
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-05-02 16:05:06 -0400

    popover bg change

--- components/logs/comparison-popover.tsx
+++ components/logs/comparison-popover.tsx
@@ -35,7 +35,7 @@ export default function ComparisonPopover({
 					<div className='whitespace-nowrap'>{value}</div>
 				</div>
 			</PopoverTrigger>
-			<PopoverContent className='flex flex-col gap-1 items-center text-xs w-48 py-2 pr-4 pl-2 bg-neutral-800'>
+			<PopoverContent className='flex flex-col gap-1 items-center text-xs w-48 py-2 pr-4 pl-2 bg-green-900'>
 				<div className='flex flex-row gap-1 items-center'>
 					<div className='capitalize'>{field}</div>
 					<div>{unitLabel}</div>
