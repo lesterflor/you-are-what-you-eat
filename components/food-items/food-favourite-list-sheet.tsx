5b12a44af6f0dccda9c09267d5d26210e1609b47
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-05-15 13:25:03 -0400

    fetch state

--- components/food-items/food-favourite-list-sheet.tsx
+++ components/food-items/food-favourite-list-sheet.tsx
@@ -4,6 +4,7 @@ import { getFavouriteFoods } from '@/actions/food-actions';
 import { GetFoodItem } from '@/types';
 import { useEffect, useState } from 'react';
 import { BsBookmarkStarFill } from 'react-icons/bs';
+import { ImSpinner2 } from 'react-icons/im';
 import { ScrollArea } from '../ui/scroll-area';
 import {
 	Sheet,
@@ -20,13 +21,17 @@ export default function FoodFavouriteListSheet({
 	children: React.ReactNode;
 }) {
 	const [favs, setFavs] = useState<GetFoodItem[]>();
+	const [isFetching, setIsFetching] = useState(false);
 
 	const getFavs = async () => {
+		setIsFetching(true);
 		const res = await getFavouriteFoods();
 
 		if (res.success) {
 			setFavs(res.data as GetFoodItem[]);
 		}
+
+		setIsFetching(false);
 	};
 
 	useEffect(() => {
@@ -35,31 +40,36 @@ export default function FoodFavouriteListSheet({
 
 	return (
 		<>
-			{favs && favs.length > 0 ? (
-				<Sheet>
-					<SheetTrigger asChild>{children}</SheetTrigger>
+			<Sheet>
+				<SheetTrigger asChild>{children}</SheetTrigger>
 
-					<SheetContent
-						side={'left'}
-						className='max-w-[100vw] w-full px-2'>
-						<SheetTitle className='flex flex-row items-center gap-2'>
-							<BsBookmarkStarFill className='text-teal-600 w-6 h-6' /> Your
-							Favourite Foods
-						</SheetTitle>
-						<SheetDescription></SheetDescription>
+				<SheetContent
+					side={'left'}
+					className='max-w-[100vw] w-full px-2'>
+					<SheetTitle className='flex flex-row items-center gap-2'>
+						<BsBookmarkStarFill className='text-teal-600 w-6 h-6' /> Your
+						Favourite Foods
+					</SheetTitle>
+					<SheetDescription></SheetDescription>
+
+					{isFetching ? (
+						<ImSpinner2 className='animate-spin w-6 h-6 opacity-20' />
+					) : (
 						<ScrollArea className='w-full pt-4 pr-3'>
-							<div className='max-h-[75vh] flex flex-col items-center gap-4'>
-								{favs.map((item) => (
-									<FoodItemCard
-										item={item}
-										key={item.id}
-									/>
-								))}
-							</div>
+							{favs && favs.length > 0 && (
+								<div className='max-h-[75vh] flex flex-col items-center gap-4'>
+									{favs.map((item) => (
+										<FoodItemCard
+											item={item}
+											key={item.id}
+										/>
+									))}
+								</div>
+							)}
 						</ScrollArea>
-					</SheetContent>
-				</Sheet>
-			) : null}
+					)}
+				</SheetContent>
+			</Sheet>
 		</>
 	);
 }
