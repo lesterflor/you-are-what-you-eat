51361136de223cb4280078b602f09569df83d550
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-07-21 15:37:05 -0400

    test update

--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -2,6 +2,6 @@ import { describe, expect, test } from '@jest/globals';
 
 describe('sum module', () => {
 	test('adds 1 + 2 to equal 3', () => {
-		expect(1 + 3).toBe(3);
+		expect(1 + 2).toBe(3);
 	});
 });
