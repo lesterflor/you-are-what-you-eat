b038a27a4c42b4dcca9898e1bda517bd501e76e7
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-07-30 17:36:45 -0400

    install storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite';
+
+import path from 'path';
+
+const getAbsolutePath = (packageName: string): any =>
+	path
+		.dirname(require.resolve(path.join(packageName, 'package.json')))
+		.replace(/^file:\/\//, '');
+
+const config: StorybookConfig = {
+	stories: [
+		'../stories/**/*.mdx',
+		'../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+	],
+	addons: [
+		getAbsolutePath('@chromatic-com/storybook'),
+		getAbsolutePath('@storybook/addon-docs'),
+		getAbsolutePath('@storybook/addon-onboarding'),
+		getAbsolutePath('@storybook/addon-a11y'),
+		getAbsolutePath('@storybook/addon-vitest')
+	],
+	framework: {
+		name: getAbsolutePath('@storybook/nextjs-vite'),
+		options: {}
+	},
+	staticDirs: ['..\\public']
+};
+export default config;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -9,17 +12,14 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname
 });
 
-const eslintConfig = [
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	{
-		rules: {
-			'@typescript-eslint/no-explicit-any': 0,
-			'@typescript-eslint/no-unused-vars': 'warn',
-			'@typescript-eslint/no-require-imports': 0,
-			'react-hooks/exhaustive-deps': 0,
-			'@typescript-eslint/ban-ts-comment': 0
-		}
-	}
-];
+const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
+    rules: {
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-require-imports': 0,
+        'react-hooks/exhaustive-deps': 0,
+        '@typescript-eslint/ban-ts-comment': 0
+    }
+}, ...storybook.configs["flat/recommended"]];
 
 export default eslintConfig;
