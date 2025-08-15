64045c0342e5bd2996e3220b217b09b4538a8b56
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-08-15 13:14:28 -0400

    add more cal burned presets

--- components/logs/expended-calories-button.tsx
+++ components/logs/expended-calories-button.tsx
@@ -10,8 +10,10 @@ import {
 	selectStatus
 } from '@/lib/features/log/logFoodSlice';
 import { useAppDispatch, useAppSelector } from '@/lib/hooks';
-import { ChevronLeft, ChevronRight, Flame, Plus } from 'lucide-react';
+import { ChevronLeft, ChevronRight, Flame, InfoIcon, Plus } from 'lucide-react';
 import { useEffect, useOptimistic, useState, useTransition } from 'react';
+import { FaWalking } from 'react-icons/fa';
+import { GiWeightLiftingUp } from 'react-icons/gi';
 import { ImSpinner2 } from 'react-icons/im';
 import { toast } from 'sonner';
 import IncrementButton from '../increment-button';
@@ -42,6 +44,12 @@ export default function ExpendedCaloriesButton({
 	const [fetching, startFetchingTransition] = useTransition();
 	const [inputVal, setInputVal] = useState(10);
 	const [popoverOpen, setPopoverOpen] = useState(false);
+
+	useEffect(() => {
+		if (!popoverOpen) {
+			setInputVal(10);
+		}
+	}, [popoverOpen]);
 
 	const getKnownCalories = async () => {
 		startFetchingTransition(async () => {
@@ -79,7 +87,22 @@ export default function ExpendedCaloriesButton({
 					)}
 				</div>
 			</PopoverTrigger>
-			<PopoverContent className='flex flex-col gap-6 items-center justify-center select-none'>
+			<PopoverContent className='flex flex-col gap-6 items-center justify-center select-none relative w-[85vw] bg-slate-950'>
+				<Popover>
+					<PopoverTrigger asChild>
+						<div className='absolute top-2 left-2'>
+							<InfoIcon className='w-6 h-6 opacity-30' />
+						</div>
+					</PopoverTrigger>
+					<PopoverContent>
+						<div className='text-muted-foreground text-xs select-none'>
+							* If you are aware of calories you have burned outside your BMR
+							(Base Metabolic Rate), enter it here. You can add more at any time
+							to keep adding to the calories you have burned throughout the day.
+						</div>
+					</PopoverContent>
+				</Popover>
+
 				<div className='flex flex-row items-center gap-2'>
 					<Flame className='w-6 h-6' />
 					Expended Calories
@@ -129,11 +152,21 @@ export default function ExpendedCaloriesButton({
 					</div>
 				</div>
 
-				<div className='flex flex-row gap-2 select-none'>
+				<div className='flex flex-row flex-wrap gap-2 select-none'>
 					<Button
 						variant={'secondary'}
-						onClick={() => setInputVal(350)}>
-						350
+						onClick={() => setInputVal(100)}>
+						100
+					</Button>
+					<Button
+						variant={'secondary'}
+						onClick={() => setInputVal(150)}>
+						150
+					</Button>
+					<Button
+						variant={'secondary'}
+						onClick={() => setInputVal(250)}>
+						250
 					</Button>
 					<Button
 						variant={'secondary'}
@@ -150,11 +183,52 @@ export default function ExpendedCaloriesButton({
 						onClick={() => setInputVal(1050)}>
 						1050
 					</Button>
+					<Button
+						className='px-1.5 gap-1'
+						variant={'secondary'}
+						onClick={() => setInputVal(350)}>
+						<FaWalking />
+						<span className='text-muted-foreground text-xs'>
+							(1 H, 5 kph)
+						</span>{' '}
+						350
+					</Button>
+					<Button
+						className='px-1.5 gap-1'
+						variant={'secondary'}
+						onClick={() => setInputVal(370)}>
+						<FaWalking />
+						<span className='text-muted-foreground text-xs'>
+							(1 H, 5.5 kph)
+						</span>{' '}
+						370
+					</Button>
+
+					<Button
+						className='px-1.5 gap-1'
+						variant={'secondary'}
+						onClick={() => setInputVal(200)}>
+						<GiWeightLiftingUp />
+						<span className='text-muted-foreground text-xs'>
+							(1 H, light)
+						</span>{' '}
+						200
+					</Button>
+					<Button
+						className='px-1.5 gap-1'
+						variant={'secondary'}
+						onClick={() => setInputVal(300)}>
+						<GiWeightLiftingUp />
+						<span className='text-muted-foreground text-xs'>
+							(1 H, mod)
+						</span>{' '}
+						300
+					</Button>
 				</div>
 
 				<Button
 					className='select-none'
-					disabled={isPending}
+					disabled={isPending || inputVal === 0}
 					onClick={() => {
 						startPendingTransition(async () => {
 							setOptimisticCalories(inputVal);
@@ -186,12 +260,6 @@ export default function ExpendedCaloriesButton({
 					)}
 					Add
 				</Button>
-
-				<div className='text-muted-foreground text-xs select-none'>
-					* If you are aware of calories you have burned outside your BMR (Base
-					Metabolic Rate), enter it here. You can add more at any time to keep
-					adding to the calories you have burned throughout the day.
-				</div>
 			</PopoverContent>
 		</Popover>
 	);
