bebc30c8d3b8fc6e20195f4c2876d5a16f786e31
lesterflor <155178241+lesterflor@users.noreply.github.com>
2025-09-28 12:42:33 -0400

    number-incrementor test updates

--- components/number-incrementor.test.tsx
+++ components/number-incrementor.test.tsx
@@ -2,296 +2,328 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NumberIncrementor from './number-incrementor';
 
-test('number incrementor default value', () => {
-	const mockFn = jest.fn((val: number) => val);
+async function longPress(target: string) {
+	const myTarget = await screen.getByTestId(target);
+	const user = userEvent.setup();
+	await user.pointer({
+		keys: '[MouseLeft>]',
+		target: myTarget
+	});
+	await new Promise((resolve) => {
+		setTimeout(resolve, 1000);
+	});
+	await user.pointer({ keys: '[/MouseLeft]', target: myTarget });
+}
 
-	render(<NumberIncrementor onChange={mockFn} />);
+describe('NumberIncrementor (Jest)', () => {
+	it('should increment more than 1 on long press', async () => {
+		const mockFn = jest.fn((val: number) => val);
 
-	const value = screen.getByTestId('current-value');
+		render(<NumberIncrementor onChange={mockFn} />);
 
-	expect(value.textContent).toBe('0');
-});
+		const value = screen.getByTestId('current-value');
 
-test('increments value when increment button is clicked', async () => {
-	const handleChange = jest.fn((val: number) => val);
+		// Initial state
+		expect(value.textContent).toBe('0');
 
-	render(
-		<NumberIncrementor
-			minValue={0}
-			maxValue={10}
-			onChange={handleChange}
-		/>
-	);
+		// long press
+		await longPress('increment-button');
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const incrementBtn = screen.getByTestId('increment-button');
+		// increment should be more than just 1
+		expect(parseInt(value.textContent)).toBeGreaterThan(1);
+	});
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('0');
+	it('number incrementor default value', () => {
+		const mockFn = jest.fn((val: number) => val);
 
-	// Click increment button once
-	await userEvent.click(incrementBtn);
+		render(<NumberIncrementor onChange={mockFn} />);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('1');
+		const value = screen.getByTestId('current-value');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(1);
-});
+		expect(value.textContent).toBe('0');
+	});
 
-test('increments value when decimcal increment button is clicked', async () => {
-	const handleChange = jest.fn((val: number) => val);
+	it('increments value when increment button is clicked', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	render(
-		<NumberIncrementor
-			minValue={0}
-			maxValue={10}
-			onChange={handleChange}
-		/>
-	);
+		render(
+			<NumberIncrementor
+				minValue={0}
+				maxValue={10}
+				onChange={handleChange}
+			/>
+		);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const incrementBtn = screen.getByTestId('increment-button-decimal');
+		const valueDisplay = screen.getByTestId('current-value');
+		const incrementBtn = screen.getByTestId('increment-button');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('0');
+		// Initial state
+		expect(valueDisplay.textContent).toBe('0');
 
-	// Click increment button once
-	await userEvent.click(incrementBtn);
+		// Click increment button once
+		await userEvent.click(incrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0.1');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('1');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(0.1);
-});
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(1);
+	});
 
-test('increments value stops at maxValue', async () => {
-	const handleChange = jest.fn((val: number) => val);
+	it('increments value when decimcal increment button is clicked', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	render(
-		<NumberIncrementor
-			minValue={0}
-			maxValue={2}
-			onChange={handleChange}
-		/>
-	);
+		render(
+			<NumberIncrementor
+				minValue={0}
+				maxValue={10}
+				onChange={handleChange}
+			/>
+		);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const incrementBtn = screen.getByTestId('increment-button');
+		const valueDisplay = screen.getByTestId('current-value');
+		const incrementBtn = screen.getByTestId('increment-button-decimal');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('0');
+		// Initial state
+		expect(valueDisplay.textContent).toBe('0');
 
-	// Click increment button once
-	await userEvent.click(incrementBtn);
+		// Click increment button once
+		await userEvent.click(incrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('1');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0.1');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(1);
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(0.1);
+	});
 
-	// Click increment button again
-	await userEvent.click(incrementBtn);
+	it('increments value stops at maxValue', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('2');
+		render(
+			<NumberIncrementor
+				minValue={0}
+				maxValue={2}
+				onChange={handleChange}
+			/>
+		);
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(2);
+		const valueDisplay = screen.getByTestId('current-value');
+		const incrementBtn = screen.getByTestId('increment-button');
 
-	// Click increment button again from 2
-	await userEvent.click(incrementBtn);
+		// Initial state
+		expect(valueDisplay.textContent).toBe('0');
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('2');
+		// Click increment button once
+		await userEvent.click(incrementBtn);
 
-	// onChange is called with same value as maxValue is 2
-	expect(handleChange).toHaveBeenCalledWith(2);
-});
+		// UI updates
+		expect(valueDisplay.textContent).toBe('1');
 
-test('increments value stops at maxValue for increment-button-decimal', async () => {
-	const handleChange = jest.fn((val: number) => val);
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(1);
 
-	render(
-		<NumberIncrementor
-			value={0.8}
-			maxValue={1}
-			onChange={handleChange}
-		/>
-	);
+		// Click increment button again
+		await userEvent.click(incrementBtn);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const incrementBtn = screen.getByTestId('increment-button-decimal');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('2');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('0.8');
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(2);
 
-	// Click increment button once
-	await userEvent.click(incrementBtn);
+		// Click increment button again from 2
+		await userEvent.click(incrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0.9');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('2');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(0.9);
+		// onChange is called with same value as maxValue is 2
+		expect(handleChange).toHaveBeenCalledWith(2);
+	});
 
-	// Click increment button again
-	await userEvent.click(incrementBtn);
+	it('increments value stops at maxValue for increment-button-decimal', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('1');
+		render(
+			<NumberIncrementor
+				value={0.8}
+				maxValue={1}
+				onChange={handleChange}
+			/>
+		);
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(1);
+		const valueDisplay = screen.getByTestId('current-value');
+		const incrementBtn = screen.getByTestId('increment-button-decimal');
 
-	// Click increment button again from 1
-	await userEvent.click(incrementBtn);
+		// Initial state
+		expect(valueDisplay.textContent).toBe('0.8');
 
-	// UI updates should not go beyond max
-	expect(valueDisplay.textContent).toBe('1');
+		// Click increment button once
+		await userEvent.click(incrementBtn);
 
-	// onChange is called with same value as maxValue is 1
-	expect(handleChange).toHaveBeenCalledWith(1);
-});
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0.9');
 
-test('decrements value when decrement button is clicked', async () => {
-	const handleChange = jest.fn((val: number) => val);
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(0.9);
 
-	render(
-		<NumberIncrementor
-			value={5}
-			onChange={handleChange}
-		/>
-	);
+		// Click increment button again
+		await userEvent.click(incrementBtn);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const decrementBtn = screen.getByTestId('decrement-button');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('1');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('5');
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(1);
 
-	// Click decrement button once
-	await userEvent.click(decrementBtn);
+		// Click increment button again from 1
+		await userEvent.click(incrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('4');
+		// UI updates should not go beyond max
+		expect(valueDisplay.textContent).toBe('1');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(4);
-});
+		// onChange is called with same value as maxValue is 1
+		expect(handleChange).toHaveBeenCalledWith(1);
+	});
 
-test('decrements value when decimcal decrement button is clicked', async () => {
-	const handleChange = jest.fn((val: number) => val);
+	it('decrements value when decrement button is clicked', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	render(
-		<NumberIncrementor
-			value={5}
-			onChange={handleChange}
-		/>
-	);
+		render(
+			<NumberIncrementor
+				value={5}
+				onChange={handleChange}
+			/>
+		);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const decrementBtn = screen.getByTestId('decrement-button-decimal');
+		const valueDisplay = screen.getByTestId('current-value');
+		const decrementBtn = screen.getByTestId('decrement-button');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('5');
+		// Initial state
+		expect(valueDisplay.textContent).toBe('5');
 
-	// Click decrement button once
-	await userEvent.click(decrementBtn);
+		// Click decrement button once
+		await userEvent.click(decrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('4.9');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('4');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(4.9);
-});
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(4);
+	});
 
-test('decrements value should not go lower than minValue', async () => {
-	const handleChange = jest.fn((val: number) => val);
+	it('decrements value when decimcal decrement button is clicked', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	render(
-		<NumberIncrementor
-			value={1}
-			minValue={0}
-			onChange={handleChange}
-		/>
-	);
+		render(
+			<NumberIncrementor
+				value={5}
+				onChange={handleChange}
+			/>
+		);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const decrementBtn = screen.getByTestId('decrement-button');
+		const valueDisplay = screen.getByTestId('current-value');
+		const decrementBtn = screen.getByTestId('decrement-button-decimal');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('1');
+		// Initial state
+		expect(valueDisplay.textContent).toBe('5');
 
-	// Click decrement button once
-	await userEvent.click(decrementBtn);
+		// Click decrement button once
+		await userEvent.click(decrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('4.9');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(0);
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(4.9);
+	});
 
-	// Click decrement button again from 0
-	await userEvent.click(decrementBtn);
+	it('decrements value should not go lower than minValue', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0');
+		render(
+			<NumberIncrementor
+				value={1}
+				minValue={0}
+				onChange={handleChange}
+			/>
+		);
 
-	// onChange is called with same value as min is 0
-	expect(handleChange).toHaveBeenCalledWith(0);
-});
+		const valueDisplay = screen.getByTestId('current-value');
+		const decrementBtn = screen.getByTestId('decrement-button');
 
-test('decrements value should not go lower than minValue with decrement-decimal button', async () => {
-	const handleChange = jest.fn((val: number) => val);
+		// Initial state
+		expect(valueDisplay.textContent).toBe('1');
 
-	render(
-		<NumberIncrementor
-			value={0.5}
-			minValue={0}
-			onChange={handleChange}
-		/>
-	);
+		// Click decrement button once
+		await userEvent.click(decrementBtn);
 
-	const valueDisplay = screen.getByTestId('current-value');
-	const decrementBtn = screen.getByTestId('decrement-button-decimal');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0');
 
-	// Initial state
-	expect(valueDisplay.textContent).toBe('0.5');
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(0);
 
-	// Click decrement button once
-	await userEvent.click(decrementBtn);
+		// Click decrement button again from 0
+		await userEvent.click(decrementBtn);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0.4');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0');
 
-	// onChange is called with updated value
-	expect(handleChange).toHaveBeenCalledWith(0.4);
+		// onChange is called with same value as min is 0
+		expect(handleChange).toHaveBeenCalledWith(0);
+	});
 
-	// Click decrement button again from 0.4
-	await userEvent.click(decrementBtn);
+	it('decrements value should not go lower than minValue with decrement-decimal button', async () => {
+		const handleChange = jest.fn((val: number) => val);
 
-	// UI updates
-	expect(valueDisplay.textContent).toBe('0.3');
+		render(
+			<NumberIncrementor
+				value={0.5}
+				minValue={0}
+				onChange={handleChange}
+			/>
+		);
 
-	// onChange is called with same value as min is 0
-	expect(handleChange).toHaveBeenCalledWith(0.3);
+		const valueDisplay = screen.getByTestId('current-value');
+		const decrementBtn = screen.getByTestId('decrement-button-decimal');
 
-	// Click decrement button again from 0.3
-	await userEvent.click(decrementBtn);
-	// Click decrement button again from 0.2
-	await userEvent.click(decrementBtn);
-	// Click decrement button again from 0.1
-	await userEvent.click(decrementBtn);
+		// Initial state
+		expect(valueDisplay.textContent).toBe('0.5');
 
-	// Click decrement button again from 0
-	await userEvent.click(decrementBtn);
+		// Click decrement button once
+		await userEvent.click(decrementBtn);
 
-	// UI updates value should not be lower than min
-	expect(valueDisplay.textContent).toBe('0');
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0.4');
 
-	// onChange is called with same value as min is 0
-	expect(handleChange).toHaveBeenCalledWith(0);
+		// onChange is called with updated value
+		expect(handleChange).toHaveBeenCalledWith(0.4);
+
+		// Click decrement button again from 0.4
+		await userEvent.click(decrementBtn);
+
+		// UI updates
+		expect(valueDisplay.textContent).toBe('0.3');
+
+		// onChange is called with same value as min is 0
+		expect(handleChange).toHaveBeenCalledWith(0.3);
+
+		// Click decrement button again from 0.3
+		await userEvent.click(decrementBtn);
+		// Click decrement button again from 0.2
+		await userEvent.click(decrementBtn);
+		// Click decrement button again from 0.1
+		await userEvent.click(decrementBtn);
+
+		// Click decrement button again from 0
+		await userEvent.click(decrementBtn);
+
+		// UI updates value should not be lower than min
+		expect(valueDisplay.textContent).toBe('0');
+
+		// onChange is called with same value as min is 0
+		expect(handleChange).toHaveBeenCalledWith(0);
+	});
 });
